13e95bbc411ba879a544f83d6630ce10a5a920de
wlsfla <lord8333@gmail.com>
2023-11-14 20:02:46 +0900

    tset feature

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # test_streamlit
  test_streamlit
+
+# Reference.
+- https://docs.streamlit.io/library/api-reference/data/st.column_config
+- https://data-editor.streamlit.app/
--- src/main.py
+++ src/main.py
@@ -1,5 +1,20 @@
 import streamlit as st
+import pandas as pd
 
-st.title('this is title')
-st.header('this is header')
-st.subheader('this is subheader')
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.header('st.button')
+
+if st.button('Say hello'):
+     st.write('Why hello there')
+else:
+     st.write('Goodbye')
+
+path = '../data/annotated.csv'
+edited_df = st.data_editor(pd.read_csv(path))
+
+favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
+st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
